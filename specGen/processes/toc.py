044214cc11f9dff362b3ca47b76b9e9cb30cf904
Geoffrey Sneddon <geoffers@gmail.com>
2008-06-15 20:54:14 +0100

    Move everything into one method

--- specGen/processes/toc.py
+++ specGen/processes/toc.py
@@ -55,109 +55,96 @@ class toc(object):
 		self.outlines = {}
 		context = etree.iterwalk(ElementTree, events=("start", "end"))
 		for action, element in context:
-			if action == "start":
-				self.start(element, **kwargs)
-			else:
-				self.end(element, **kwargs)
-	
-	def start(self, element, **kwargs):
-		# If the top of the stack is a heading content element
-		if self.stack and self.stack[-1].tag in heading_content:
-			# Do nothing.
-			return
-		
-		# When entering a sectioning content element or a sectioning root element
-		elif element.tag in sectioning_content or element.tag in sectioning_root:
-			# If current outlinee is not null, push current outlinee onto the stack.
-			if self.current_outlinee is not None:
-				self.stack.append(self.current_outlinee)
-			# Let current outlinee be the element that is being entered.
-			self.current_outlinee = element
-			# Let current section be a newly created section for the current outlinee element.
-			self.current_section = section()
-			# Let there be a new outline for the new current outlinee, initialized with just the new current section as the only section in the outline.
-			self.outlines[self.current_outlinee] = [self.current_section]
+			# If the top of the stack is an element, and you are exiting that element
+			if action == "end" and self.stack and self.stack[-1] == element:
+				# Note: The element being exited is a heading content element.
+				assert element.tag in heading_content
+				# Pop that element from the stack.
+				self.stack.pop()
 			
-		# If the current outlinee is null.
-		elif self.current_outlinee is None:
-			# Do nothing.
-			return
-		
-		# When entering a heading content element
-		elif element.tag in heading_content:
-			# If the current section has no heading, let the element being entered be the heading for the current section.
-			if self.current_section.header is None:
-				self.current_section.header = element
+			# If the top of the stack is a heading content element
+			elif self.stack and self.stack[-1].tag in heading_content:
+				# Do nothing.
+				pass
 			
-			# Otherwise, if the element being entered has a rank equal to or greater than the heading of the current section, then create a new section and append it to the outline of the current outlinee element, so that this new section is the new last section of that outline. Let current section be that new section. Let the element being entered be the new heading for the current section.
-			elif rank[element.tag] <= rank[self.current_section.header.tag]:
+			# When entering a sectioning content element or a sectioning root element
+			elif action == "start" and (element.tag in sectioning_content or element.tag in sectioning_root):
+				# If current outlinee is not null, push current outlinee onto the stack.
+				if self.current_outlinee is not None:
+					self.stack.append(self.current_outlinee)
+				# Let current outlinee be the element that is being entered.
+				self.current_outlinee = element
+				# Let current section be a newly created section for the current outlinee element.
 				self.current_section = section()
-				self.outlines[self.current_outlinee].append(self.current_section)
-				self.current_section.header = element
-			
-			# Otherwise, run these substeps:
-			else:
-				# Let candidate section be current section.
-				candidate_section = self.current_section
-				while True:
-					# If the element being entered has a rank lower than the rank of the heading of the candidate section, then create a new section, and append it to candidate section. (This does not change which section is the last section in the outline.) Let current section be this new section. Let the element being entered be the new heading for the current section. Abort these substeps.
-					if rank[element.tag] > rank[candidate_section.header.tag]:
-						self.current_section = section()
-						candidate_section.subsections.append(self.current_section)
-						self.current_section.header = element
-						break
-					# Let new candidate section be the section that contains candidate section in the outline of current outlinee.
-					for each_section in self.outlines[self.current_outlinee]:
-						if candidate_section in each_section.subsections:
-							new_candidate_section = each_section
-							break
-					# Let candidate section be new candidate section.
-					candidate_section = new_candidate_section
-					# Return to step 2.
-			# Push the element being entered onto the stack. (This causes the algorithm to skip any descendants of the element.)
-			self.stack.append(element)
-	
-	def end(self, element, **kwargs):
-		# If the top of the stack is an element, and you are exiting that element
-		if self.stack and self.stack[-1] == element:
-			# Note: The element being exited is a heading content element.
-			assert element.tag in heading_content
-			# Pop that element from the stack.
-			self.stack.pop()
-			
-		# If the top of the stack is a heading content element
-		elif self.stack and self.stack[-1].tag in heading_content:
-			# Do nothing.
-			return
-			
-		# When exiting a sectioning content element, if the stack is not empty
-		elif element.tag in sectioning_content and self.stack:
-			# Pop the top element from the stack, and let the current outlinee be that element.
-			self.current_outlinee = self.stack.pop()
-			# Let current section be the last section in the outline of the current outlinee element.
-			self.current_section = self.outlines[self.current_outlinee][-1]
-			# Append the outline of the sectioning content element being exited to the current section. (This does not change which section is the last section in the outline.)
-			self.current_section.subsections += self.outlines[element]
-			
-		# When exiting a sectioning root element, if the stack is not empty
-		elif element.tag in sectioning_root and self.stack:
-			# Pop the top element from the stack, and let the current outlinee be that element.
-			self.current_outlinee = self.stack.pop()
-			# Let current section be the last section in the outline of the current outlinee element.
-			self.current_section = self.outlines[self.current_outlinee][-1]
-			# Loop: If current section has no child sections, stop these steps.
-			while self.current_section.subsections:
-				# Let current section be the last child section of the current current section.
-				assert self.current_section != self.current_section.subsections[-1]
-				self.current_section = self.current_section.subsections[-1]
-				# Go back to the substep labeled Loop.
+				# Let there be a new outline for the new current outlinee, initialized with just the new current section as the only section in the outline.
+				self.outlines[self.current_outlinee] = [self.current_section]
 				
-		# When exiting a sectioning content element or a sectioning root element
-		elif element.tag in sectioning_content or element.tag in sectioning_root:
-			# Note: The current outlinee is the element being exited.
-			assert self.current_outlinee == element
-			# Let current section be the first section in the outline of the current outlinee element.
-			self.current_section = self.outlines[self.current_outlinee][0]
-			# Skip to the next step in the overall set of steps. (The walk is over.)
-			# TODO: Implement this somehow.
-			assert False and "This is yet to be implemented"
+			# When exiting a sectioning content element, if the stack is not empty
+			elif action == "end" and element.tag in sectioning_content and self.stack:
+				# Pop the top element from the stack, and let the current outlinee be that element.
+				self.current_outlinee = self.stack.pop()
+				# Let current section be the last section in the outline of the current outlinee element.
+				self.current_section = self.outlines[self.current_outlinee][-1]
+				# Append the outline of the sectioning content element being exited to the current section. (This does not change which section is the last section in the outline.)
+				self.current_section.subsections += self.outlines[element]
+				
+			# When exiting a sectioning root element, if the stack is not empty
+			elif action == "end" and element.tag in sectioning_root and self.stack:
+				# Pop the top element from the stack, and let the current outlinee be that element.
+				self.current_outlinee = self.stack.pop()
+				# Let current section be the last section in the outline of the current outlinee element.
+				self.current_section = self.outlines[self.current_outlinee][-1]
+				# Loop: If current section has no child sections, stop these steps.
+				while self.current_section.subsections:
+					# Let current section be the last child section of the current current section.
+					assert self.current_section != self.current_section.subsections[-1]
+					self.current_section = self.current_section.subsections[-1]
+					# Go back to the substep labeled Loop.
+					
+			# When exiting a sectioning content element or a sectioning root element
+			elif action == "end" and (element.tag in sectioning_content or element.tag in sectioning_root):
+				# Note: The current outlinee is the element being exited.
+				assert self.current_outlinee == element
+				# Let current section be the first section in the outline of the current outlinee element.
+				self.current_section = self.outlines[self.current_outlinee][0]
+				# Skip to the next step in the overall set of steps. (The walk is over.)
+				break
+				
+			# If the current outlinee is null.
+			elif self.current_outlinee is None:
+				# Do nothing.
+				pass
+			
+			# When entering a heading content element
+			elif action == "start" and element.tag in heading_content:
+				# If the current section has no heading, let the element being entered be the heading for the current section.
+				if self.current_section.header is None:
+					self.current_section.header = element
+				
+				# Otherwise, if the element being entered has a rank equal to or greater than the heading of the current section, then create a new section and append it to the outline of the current outlinee element, so that this new section is the new last section of that outline. Let current section be that new section. Let the element being entered be the new heading for the current section.
+				elif rank[element.tag] <= rank[self.current_section.header.tag]:
+					self.current_section = section()
+					self.outlines[self.current_outlinee].append(self.current_section)
+					self.current_section.header = element
+				
+				# Otherwise, run these substeps:
+				else:
+					# Let candidate section be current section.
+					candidate_section = self.current_section
+					while True:
+						# If the element being entered has a rank lower than the rank of the heading of the candidate section, then create a new section, and append it to candidate section. (This does not change which section is the last section in the outline.) Let current section be this new section. Let the element being entered be the new heading for the current section. Abort these substeps.
+						if rank[element.tag] > rank[candidate_section.header.tag]:
+							self.current_section = section()
+							candidate_section.subsections.append(self.current_section)
+							self.current_section.header = element
+							break
+						# Let new candidate section be the section that contains candidate section in the outline of current outlinee.
+						for each_section in self.outlines[self.current_outlinee]:
+							if candidate_section in each_section.subsections:
+								new_candidate_section = each_section
+								break
+						# Let candidate section be new candidate section.
+						candidate_section = new_candidate_section
+						# Return to step 2.
+				# Push the element being entered onto the stack. (This causes the algorithm to skip any descendants of the element.)
+				self.stack.append(element)
