cc4cf982c474bf9ff555fe1801bcb352c8f8253e
Geoffrey Sneddon <geoffers@gmail.com>
2008-07-05 15:32:05 +0100

    Allow min/max depth to be set

--- specGen/processes/toc.py
+++ specGen/processes/toc.py
@@ -35,7 +35,7 @@ class toc(object):
 		self.buildToc(ElementTree, **kwargs)
 		self.addToc(ElementTree, **kwargs)
 	
-	def buildToc(self, ElementTree, **kwargs):
+	def buildToc(self, ElementTree, min_depth = 2, max_depth = 6, **kwargs):
 		# Build the outline of the document
 		outline_creator = outliner.Outliner()
 		outline = outline_creator.build(ElementTree)
@@ -51,58 +51,63 @@ class toc(object):
 			# Get the section and depth at the end of list
 			section, depth = sections.pop()
 			
-			# Numbering:
-			# No children, no sibling, move back to parent's sibling
-			if depth + 1 < len(num):
-				del num[depth + 1:]
-			# Children
-			elif depth == len(num):
-				num.append(0)
-			
-			# If we have a header
-			if section.header is not None:
-				# Add ID to header
-				id = utils.generateID(section.header)
-				section.header.set("id", id)
-				# Add number, if @class doesn't contain no-num
-				if not utils.elementHasClass(section.header, "no-num"):
-					num[-1] += 1
-					section.header[0:0] = [etree.Element("span", {"class": "secno"})]
-					section.header[0].tail = section.header.text
-					section.header.text = None
-					section.header[0].text = u".".join(map(str, num)) + u". "
-				# Add to TOC, if @class doesn't contain no-toc
-				if not utils.elementHasClass(section.header, "no-toc"):
-					# Find the appropriate section of the TOC 
-					i = 0
-					toc_section = self.toc
-					while i < depth:
-						try:
-							# If the final li has no children, or the last children isn't an ol element
-							if len(toc_section[-1]) == 0 or toc_section[-1][-1].tag != "ol":
-								toc_section[-1].append(etree.Element("ol"))
-						except IndexError:
-							# If the current ol has no li in it
-							toc_section.append(etree.Element("li"))
-							toc_section[0].append(etree.Element("ol"))
-						# TOC Section is now the final child (ol) of the final item (li) in the previous section
-						assert toc_section[-1].tag == "li"
-						assert toc_section[-1][-1].tag == "ol"
-						toc_section = toc_section[-1][-1]
-						i += 1
-					# Add the current item to the TOC
-					item = etree.Element("li")
-					toc_section.append(item)
-					link = deepcopy(section.header)
-					item.append(link)
-					# Make it link to the header
-					link.tag = "a"
-					link.set("href", "#" + id)
-					# TODO: Need to remove @id and @class, not set it to an empty string
-					link.set("id", "")
-					link.set("class", "")
-					# We don't want the old tail (or any tail, for that matter)
-					link.tail = None
+			# Check we're in the valid depth range (min/max_depth are 1 based, depth is 0 based)
+			if depth >= min_depth - 1 and depth <= max_depth - 1:
+				# Calculate the corrected depth (i.e., the actual depth within the numbering/TOC)
+				corrected_depth = depth - min_depth + 1
+				
+				# Numbering:
+				# No children, no sibling, move back to parent's sibling
+				if corrected_depth + 1 < len(num):
+					del num[corrected_depth + 1:]
+				# Children
+				elif corrected_depth == len(num):
+					num.append(0)
+				
+				# If we have a header
+				if section.header is not None:
+					# Add ID to header
+					id = utils.generateID(section.header)
+					section.header.set("id", id)
+					# Add number, if @class doesn't contain no-num
+					if not utils.elementHasClass(section.header, "no-num"):
+						num[-1] += 1
+						section.header[0:0] = [etree.Element("span", {"class": "secno"})]
+						section.header[0].tail = section.header.text
+						section.header.text = None
+						section.header[0].text = u".".join(map(str, num)) + u". "
+					# Add to TOC, if @class doesn't contain no-toc
+					if not utils.elementHasClass(section.header, "no-toc"):
+						# Find the appropriate section of the TOC 
+						i = 0
+						toc_section = self.toc
+						while i < corrected_depth:
+							try:
+								# If the final li has no children, or the last children isn't an ol element
+								if len(toc_section[-1]) == 0 or toc_section[-1][-1].tag != "ol":
+									toc_section[-1].append(etree.Element("ol"))
+							except IndexError:
+								# If the current ol has no li in it
+								toc_section.append(etree.Element("li"))
+								toc_section[0].append(etree.Element("ol"))
+							# TOC Section is now the final child (ol) of the final item (li) in the previous section
+							assert toc_section[-1].tag == "li"
+							assert toc_section[-1][-1].tag == "ol"
+							toc_section = toc_section[-1][-1]
+							i += 1
+						# Add the current item to the TOC
+						item = etree.Element("li")
+						toc_section.append(item)
+						link = deepcopy(section.header)
+						item.append(link)
+						# Make it link to the header
+						link.tag = "a"
+						link.set("href", "#" + id)
+						# TODO: Need to remove @id and @class, not set it to an empty string
+						link.set("id", "")
+						link.set("class", "")
+						# We don't want the old tail (or any tail, for that matter)
+						link.tail = None
 			# Add subsections in reverse order (so the next one is executed next) with a higher depth value
 			sections.extend((child_section, depth + 1) for child_section in reversed(section))
 	
