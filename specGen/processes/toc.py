3ebac1cd77c508c89ce2969c5d008ddd28dedc8b
Geoffrey Sneddon <geoffers@gmail.com>
2008-07-26 21:46:04 +0100

    Make top-level headers not come out in reverse order for their numbers and their TOC location, make stuff work in a sane way with headerless but childful sections.

--- specGen/processes/toc.py
+++ specGen/processes/toc.py
@@ -44,7 +44,7 @@ class toc(object):
 		outline = outline_creator.build(ElementTree)
 		
 		# Get a list of all the top level sections, and their depth (0)
-		sections = [(section, 0) for section in outline]
+		sections = [(section, 0) for section in reversed(outline)]
 		
 		# Numbering
 		num = []
@@ -70,6 +70,37 @@ class toc(object):
 				elif corrected_depth == len(num):
 					num.append(0)
 				
+				# Increment the current section's number
+				if section.header is not None and not utils.elementHasClass(section.header, "no-num") or section.header is None and section:
+					num[-1] += 1
+				
+				# Get the current TOC section for this depth, and add another item to it
+				if section.header is not None and not utils.elementHasClass(section.header, "no-toc") or section.header is None and section:
+					# Find the appropriate section of the TOC 
+					i = 0
+					toc_section = self.toc
+					while i < corrected_depth:
+						try:
+							# If the final li has no children, or the last children isn't an ol element
+							if len(toc_section[-1]) == 0 or toc_section[-1][-1].tag != "ol":
+								toc_section[-1].append(etree.Element("ol"))
+								if w3c_compat or w3c_compat_class_toc:
+									toc_section[-1][-1].set("class", "toc")
+						except IndexError:
+							# If the current ol has no li in it
+							toc_section.append(etree.Element("li"))
+							toc_section[0].append(etree.Element("ol"))
+							if w3c_compat or w3c_compat_class_toc:
+								toc_section[0][-1].set("class", "toc")
+						# TOC Section is now the final child (ol) of the final item (li) in the previous section
+						assert toc_section[-1].tag == "li"
+						assert toc_section[-1][-1].tag == "ol"
+						toc_section = toc_section[-1][-1]
+						i += 1
+					# Add the current item to the TOC
+					item = etree.Element("li")
+					toc_section.append(item)
+					
 				# If we have a header
 				if section.header is not None:
 					# Add ID to header
@@ -77,7 +108,6 @@ class toc(object):
 					section.header.set("id", id)
 					# Add number, if @class doesn't contain no-num
 					if not utils.elementHasClass(section.header, "no-num"):
-						num[-1] += 1
 						section.header[0:0] = [etree.Element("span", {"class": "secno"})]
 						section.header[0].tail = section.header.text
 						section.header.text = None
@@ -85,30 +115,6 @@ class toc(object):
 						section.header[0].text += u" "
 					# Add to TOC, if @class doesn't contain no-toc
 					if not utils.elementHasClass(section.header, "no-toc"):
-						# Find the appropriate section of the TOC 
-						i = 0
-						toc_section = self.toc
-						while i < corrected_depth:
-							try:
-								# If the final li has no children, or the last children isn't an ol element
-								if len(toc_section[-1]) == 0 or toc_section[-1][-1].tag != "ol":
-									toc_section[-1].append(etree.Element("ol"))
-									if w3c_compat or w3c_compat_class_toc:
-										toc_section[-1][-1].set("class", "toc")
-							except IndexError:
-								# If the current ol has no li in it
-								toc_section.append(etree.Element("li"))
-								toc_section[0].append(etree.Element("ol"))
-								if w3c_compat or w3c_compat_class_toc:
-									toc_section[0][-1].set("class", "toc")
-							# TOC Section is now the final child (ol) of the final item (li) in the previous section
-							assert toc_section[-1].tag == "li"
-							assert toc_section[-1][-1].tag == "ol"
-							toc_section = toc_section[-1][-1]
-							i += 1
-						# Add the current item to the TOC
-						item = etree.Element("li")
-						toc_section.append(item)
 						link = deepcopy(section.header)
 						item.append(link)
 						# Make it link to the header
