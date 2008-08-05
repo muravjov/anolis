03ba0c0df754ce1ea5f68f41370a5ecf147566aa
Geoffrey Sneddon <geoffers@gmail.com>
2008-08-05 22:44:26 +0200

    Bump to 1.0RC1-dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name = "specGen",
 	license="""MIT""",
-	version = "1.0b2",
+	version = "1.0RC1-dev",
 	author = "Geoffrey Sneddon",
 	author_email = "geoffers@gmail.com",
 	packages = ["specGen", "specGen/processes"],
