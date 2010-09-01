8433ac8d5f7c4e38473affc4c9b7e3123960ac46
Michael(tm) Smith <sideshowbarker@gmail.com>
2010-09-01 14:34:01 +0200

    Add terms.py

--- anolislib/processes/filter.py
+++ anolislib/processes/filter.py
@@ -5,23 +5,4 @@ def filter(ElementTree, **kwargs):
         return
     selector = cssselect.CSSSelector(kwargs["filter"])
     for element in selector(ElementTree.getroot()):
-        remove(element)
-
-def remove(element):
-    if element.tail:
-        if element.getprevious() is not None:
-            target = element.getprevious()
-            if target.tail:
-                target.tail += element.tail
-            else:
-                target.tail = element.tail
-        else:
-            target = element.getparent()
-            if target.text:
-                target.text += element.text
-            else:
-                target.text = element.text
-        
-    element.getparent().remove(element)
-
-        
+        element.drop_tree()
