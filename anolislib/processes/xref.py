4b81a11d759d7fbdf39da0f4032b8e342ac447d3
Ms2ger <ms2ger@gmail.com>
2012-01-24 12:19:10 +0100

    Use separators argument to json.dumps.

--- anolislib/processes/xref.py
+++ anolislib/processes/xref.py
@@ -88,8 +88,7 @@ class xref(object):
         return data
 
     def dump(self, obj, f, **kwargs):
-        d = json.dumps(obj, sort_keys=True, allow_nan=False, indent=2)
-        d = d.replace(u" \n", u"\n")
+        d = json.dumps(obj, sort_keys=True, allow_nan=False, indent=2, separators=(',', ': ')))
         fp = open(f, u"wb")
         fp.write(d + u"\n")
         fp.close()
