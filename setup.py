30f76304a8e5ca3731bf87f1a58391e408990864
Ms2ger <ms2ger@gmail.com>
2012-10-27 14:18:48 +0200

    Remove print statement.

--- setup.py
+++ setup.py
@@ -19,13 +19,14 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
 import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 requires = [line for line in open(os.path.join(here, 'requirements.txt'))
             if line.strip()]
-print requires
+
 setup(
     # Basic project info
     name = "anolis",
